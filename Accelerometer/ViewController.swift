9f81a08776b54cb02240be9661332b06459da4ab
seongeunkim <grah.kim@gmail.com>
2018-06-19 15:08:37 -0300

    Initial Commit

---
+++ Accelerometer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Accelerometer
+//
+//  Created by Seong Eun Kim on 19/06/18.
+//  Copyright © 2018 Seong Eun Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
