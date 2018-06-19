24875801de6ceb03778cbf757cda7d3c5e7c3ba0
seongeunkim <grah.kim@gmail.com>
2018-06-19 18:11:52 -0300

    removed gyro code, bc there is no access to the gyro values in appleWatch :c accelerometer is working perfectly

--- Accelerometer WatchKit Extension/InterfaceController.swift
+++ Accelerometer WatchKit Extension/InterfaceController.swift
@@ -19,14 +19,9 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet var accY: WKInterfaceLabel!
     @IBOutlet var accZ: WKInterfaceLabel!
     
-    @IBOutlet var gyroX: WKInterfaceLabel!
-    @IBOutlet var gyroY: WKInterfaceLabel!
-    @IBOutlet var gyroZ: WKInterfaceLabel!
-    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        accX?.setText("X: 3")
         // Configure interface objects here.
     }
     
@@ -38,32 +33,13 @@ class InterfaceController: WKInterfaceController {
         if motionManager.isAccelerometerAvailable {
             motionManager.accelerometerUpdateInterval = 0.2
         }
-        //if motionManager.isGyroAvailable {
-            motionManager.gyroUpdateInterval = 0.2
-        //}
 
         // start getting updates
         motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
             if let data = data {
                 self.outputAccelerationData(acceleration: data.acceleration)
             }
-            
         }
-        
-        motionManager.startGyroUpdates(to: OperationQueue.current!) { (data, error) in
-            if let data = data {
-                self.outputGyroData(rotation: data.rotationRate)
-            }
-        }
-//        motionManager.startAccelerometerUpdates(to: OperationQueue.current!, withHandler: { (accData: CMAccelerometerData?, error: NSError?) -> Void in
-//            if let accData = accData {
-//                //self.outputAccelerationData(acceleration: accData.acceleration)
-//            }
-//        } as! CMAccelerometerHandler)
-
-//        motionManager.startGyroUpdates(to: OperationQueue.current!, withHandler: { (gyroData: CMGyroData?, error: NSError?) -> Void in
-//
-//        } as! CMGyroHandler)
     }
     
     override func didDeactivate() {
@@ -75,12 +51,6 @@ class InterfaceController: WKInterfaceController {
         accX?.setText("accX: \(acceleration.x).2fg")
         accY?.setText("accY: \(acceleration.y).2fg")
         accZ?.setText("accZ: \(acceleration.y).2fg")
-    }
-    
-    func outputGyroData(rotation: CMRotationRate) {
-        gyroX?.setText("gyroX: \(rotation.x).2fr/s")
-        gyroY?.setText("gyroY: \(rotation.y).2fr/s")
-        gyroZ?.setText("gyroZ: \(rotation.z).2fr/s")
     }
 
 }
